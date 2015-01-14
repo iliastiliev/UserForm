0317b7eb0979f5fbeb7156eba9171dbd2a97326b
Ilia Iliev <ilia_stefanov@gbg.bg>
2015-01-14 11:18:31 +0200

    Adding of additional information in the server script

--- app/app.js
+++ app/app.js
@@ -1,19 +1,22 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser');
+/*file for configuration of the http server*/
 
-app.set('view engine', 'jade');
-app.set('views', './views');
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+var app        = express();
+var express    = require('express');
+var fs         = require('fs');
+var bodyParser = require('body-parser');
 
 app.users = [
 	{username: "iliailiev", fullname: 'Ilia Iliev', email: 'iliailiev@gmail.com', phone: '+3598873856241'},
 	{username: "ivanivanov", fullname: 'Ivan Ivanov', email: 'ivanivanov@hotmail.com', phone: '+359899553322'},
 	{username: "georgigeorgiev", fullname: 'Georgi Geogiev', email: 'georgigeorgiev@yahoo.com', phone: '+359879359478'}
 ];
+
+app.set('view engine', 'jade'); // setup of main engine
+app.set('views', './views'); // setup of main view folder
+
+app.use(express.static(__dirname + '/public')); // setup of static folder
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 // dynamically include routes (Controller) 
 fs.readdirSync('./controllers').forEach(function (file) { 
@@ -23,6 +26,6 @@ fs.readdirSync('./controllers').forEach(function (file) {
 	} 
 });
 
-app.use(app.route);
+app.use(app.route); 
 
-app.listen(3000);
+app.listen(3000); // port for listening for client requests
