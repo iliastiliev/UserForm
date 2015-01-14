21390b5e52e282ff77cc0ca03274e0e3ebf025db
Ilia Iliev <ilia_stefanov@gbg.bg>
2015-01-14 11:30:01 +0200

    Adding of comments in the controller

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,26 +1,35 @@
+/*main controller from MVC model*/
+
 module.exports.controller = function(app){
+	
+	//rendered of index file and it loaded
 	app.get('/', function(req, res){ 
 		res.render('index', {users: app.users});
 	});
 
+	//creation of get request for delete of user by index
 	app.get('/delete/:id', function(req, res){
 		app.users.splice(req.params.id, 1);
 		res.redirect('/');
 	});
 
+	//creation of post request for edit of all information for user
 	app.post('/edit/:id', function(req, res){
 		app.users[req.params.id] = req.body;
-		res.redirect('/')
-	})
+		res.redirect('/');
+	});
 
+	//creation of post request for create of user
 	app.post('/create', function(req, res){
 		app.users.push(req.body);
-		res.redirect('/')
-	})
+		res.redirect('/');
+	});
 
+	//rendered of index file and show of information for finded users from creteria
 	app.post('/search', function(req, res){
 		var searchedUsers = [];
-		console.log(req.body['search-content']);
+		
+		//check for empty search creteria; if creteria exists the controller search for exact match
 		if(req.body['search-content'] === ""){
 			res.render('index', {users: app.users});
 		}
